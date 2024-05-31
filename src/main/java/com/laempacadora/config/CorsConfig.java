ce24f028929b14b242447377e8e5c65fbc2d24c8
JhonBG <jhon.botero1@udea.edu.co>
2024-05-30 23:58:44 -0500

    actualizar url-CORS

--- src/main/java/com/laempacadora/config/CorsConfig.java
+++ src/main/java/com/laempacadora/config/CorsConfig.java
@@ -14,8 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://mesa-master.vercel.app","http://localhost:3000","https://mesa-master-git-main-jhonbgs-projects.vercel.app","https://mesa-master-hkmm0ufya-jhonbgs-projects.vercel.app")
-//                https://mesa-master-hkmm0ufya-jhonbgs-projects.vercel.app/
+                        .allowedOrigins("https://mesa-master.vercel.app")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://mesa-master-git-main-jhonbgs-projects.vercel.app")
+                        .allowedOrigins("https://mesa-master-hkmm0ufya-jhonbgs-projects.vercel.app")
                         .allowedMethods("*") // Permitir todos los métodos
                         .allowCredentials(true)
                         .exposedHeaders("Authorization") // Exponer el encabezado de autorización
