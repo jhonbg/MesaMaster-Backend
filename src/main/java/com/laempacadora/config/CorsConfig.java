b7335b9514394e04b2204be1258b6bc60e076371
JhonBG <jhon.botero1@udea.edu.co>
2024-05-31 00:10:34 -0500

    actualizar url-CORS

--- src/main/java/com/laempacadora/config/CorsConfig.java
+++ src/main/java/com/laempacadora/config/CorsConfig.java
@@ -19,6 +19,7 @@ public class CorsConfig {
                         .allowedOrigins("https://mesa-master-git-main-jhonbgs-projects.vercel.app")
                         .allowedOrigins("https://mesa-master-hkmm0ufya-jhonbgs-projects.vercel.app")
                         .allowedMethods("*") // Permitir todos los métodos
+                        .allowedHeaders("X-CSRF-Token", "X-Requested-With", "Accept", "Accept-Version", "Content-Length", "Content-MD5", "Content-Type", "Date", "X-Api-Version")
                         .allowCredentials(true)
                         .exposedHeaders("Authorization") // Exponer el encabezado de autorización
                         .maxAge(3600); // Tiempo máximo de caché para preflight
